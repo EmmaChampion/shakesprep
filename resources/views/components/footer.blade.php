291dde7ce1055339306dae45c6b1ae0c811f9a2b
Emma Champion <277982908+EmmaChampion@users.noreply.github.com>
2026-08-09 15:22:27 -0400

    Add page footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer sm:footer-horizontal footer-center bg-primary text-secondary p-4">
+  <aside class="w-full">
+    <div class="flex justify-around w-full">
+        <a href="/about">About ShakesPrep</a>
+        <a href="/contact">Contact</a>
+        <a href="https://github.com/EmmaChampion/shakesprep">GitHub</a>
+    </div>
+  </aside>
+</footer>
