4c71f83df0e5ae2eeaa9019044038c5fb6735a6c
Emma Champion <277982908+EmmaChampion@users.noreply.github.com>
2026-08-09 15:27:56 -0400

    Make GitHub link open in new tab

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="flex justify-around w-full">
         <a href="/about">About ShakesPrep</a>
         <a href="/contact">Contact</a>
-        <a href="https://github.com/EmmaChampion/shakesprep">GitHub</a>
+        <a href="https://github.com/EmmaChampion/shakesprep" target="_blank">GitHub</a>
     </div>
   </aside>
 </footer>
