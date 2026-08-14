8337446fe3cad71a4663bc853cd0d8e352d84a0c
Emma Champion <277982908+EmmaChampion@users.noreply.github.com>
2026-08-14 02:43:32 -0400

    Highlight character's lines in script

--- resources/js/custom-scripts/highlightLines.js
+++ resources/js/custom-scripts/highlightLines.js
@@ -1,3 +1,37 @@
 import { playCodes } from "./playCodes.js";
 
-document.title = playCodes[window.playCode][window.characterCode] + " Script";
+const charName = playCodes[window.playCode][window.characterCode];
+document.title = charName + " Script";
+const content = document.querySelector("main");
+
+const speakers = Array.from(document.querySelectorAll(".speaker"))
+    .filter(speaker => speaker.textContent === charName.toUpperCase());
+
+let charLines = [];
+for (const speaker of speakers) {
+    let line = speaker.nextSibling;
+    //Loop through lines after current character's speaker tag until reaching the next speaker tag
+    while (line) {
+        //Only highlight character's spoken lines
+        if (line.nodeType === Node.TEXT_NODE) {
+            //Ignore blank lines
+            if (line.textContent.trim() !== "") {
+                charLines.push(line);
+            }
+        }
+        else {
+            //When encountering a line that isn't the character's, skip to their next line
+            if (line.nodeName === "SPAN" && line.classList.contains("speaker")) {
+                break;
+            }
+        }
+        line = line.nextSibling;
+    }
+}
+
+for (const line of charLines) {
+    const spanWrapper = document.createElement("span");
+    spanWrapper.style.backgroundColor= "LightPink";
+    content.replaceChild(spanWrapper, line);
+    spanWrapper.appendChild(line);
+}
